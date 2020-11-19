d8bf6ecf0aa7fd8f2e2dcaf04658489e694a6a6c
Dimuthu Weerarathne <dnwnalaka@gmail.com>
2020-11-19 23:24:14 +0800

    Updates

---
+++ precache-manifest.ec71e0f19831b19667d5cbc1786a2df1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8170ad36e176f6c7309c73445c8ab9e",
+    "url": "/react_monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3b700c857a1d4b0d7516",
+    "url": "/react_monsters-rolodex/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "49a52289c12e9f378484",
+    "url": "/react_monsters-rolodex/static/js/2.23788f6f.chunk.js"
+  },
+  {
+    "revision": "08aee544f36ba213a8865d2a35c6879d",
+    "url": "/react_monsters-rolodex/static/js/2.23788f6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b700c857a1d4b0d7516",
+    "url": "/react_monsters-rolodex/static/js/main.7a02fcab.chunk.js"
+  },
+  {
+    "revision": "8ed65bf25457e40127c9",
+    "url": "/react_monsters-rolodex/static/js/runtime-main.21377d0c.js"
+  }
+]);
